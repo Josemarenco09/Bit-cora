d4efb4258f26df6b5216420f2de8489d0ec3fae2
Jose Luis Marenco Hernandez <jm@MacBook-Neo-de-Luis.local>
2026-06-05 10:39:26 -0500

    Modificacion css de contacto, tarjetas de informacion index

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "marenco09",
-    database: "taller_fullstack"
+    database: "contactos_db"
 });
 
 // Conectar a MySQL
